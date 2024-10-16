2bb835f5a01293542a25a66c4015a9f0d3ac8b5c
thang22iteManhcongan008567 <phanhuuthang04@gmail.com>
2024-10-16 23:33:20 +0700

    init project

---
+++ cnpm_thoitrang.Module/BusinessObjects/thoitrangCode/Shop_Order.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using DevExpress.Xpo;
+using DevExpress.Xpo.Metadata;
+using DevExpress.Data.Filtering;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Reflection;
+namespace cnpm_thoitrang.Module.BusinessObjects.thoitrang
+{
+
+    public partial class Shop_Order
+    {
+        public Shop_Order(Session session) : base(session) { }
+        public override void AfterConstruction() 
+        { 
+            base.AfterConstruction(); 
+            if (Session.IsNewObject(this))
+            {
+                dateOrder = DateTime.Now;
+            }
+        }
+    }
+
+}
---
+++ cnpm_thoitrang.Module/BusinessObjects/thoitrangCode/Shopping_Cart_Item.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using DevExpress.Xpo;
+using DevExpress.Xpo.Metadata;
+using DevExpress.Data.Filtering;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Reflection;
+namespace cnpm_thoitrang.Module.BusinessObjects.thoitrang
+{
+
+    public partial class Shopping_Cart_Item
+    {
+        public Shopping_Cart_Item(Session session) : base(session) { }
+        public override void AfterConstruction() { base.AfterConstruction(); }
+    }
+
+}
